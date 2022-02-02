627a9962b258bbd6395cdb46f67ecac0c838df1e
LHERBE <72711895+IsmainL81@users.noreply.github.com>
2022-02-02 12:26:38 +0100

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ViewGraphComponent } from './view-graph/view-graph.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ViewGraphComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
